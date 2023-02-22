cb1a03b7f0b2fea2b622ae243ade1fc8de4f2bd1
Mr.Hope <mister-hope@outlook.com>
2023-02-22 15:42:57 +0800

    docs: update config

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -53,13 +53,6 @@ export default hopeTheme({
     },
   },
 
-  encrypt: {
-    config: {
-      "/demo/encrypt.html": ["1234"],
-      "/zh/demo/encrypt.html": ["1234"],
-    },
-  },
-
   plugins: {
     mdEnhance: {
       codetabs: true,

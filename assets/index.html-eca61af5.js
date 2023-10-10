24274e9974979b4f7238a0d711368f0068c7d3be
Mister-Hope <mister-hope@outlook.com>
2023-10-10 13:53:07 +0000

    Deploying to netlify from @ vuepress/docs@0d0414001c4f5c31236c7fd40ce1f98c6a4c94d9 🚀

---
+++ assets/index.html-eca61af5.js
@@ -0,0 +1,35 @@
+import{_ as t,r as c,o as r,c as o,a as d,w as e,b as s,d as n}from"./app-3137f2f5.js";const u={},m=s("h3",{id:"as-easy-as-1-2-3",tabindex:"-1"},[s("a",{class:"header-anchor",href:"#as-easy-as-1-2-3","aria-hidden":"true"},"#"),n(" As Easy as 1, 2, 3")],-1),v=s("div",{class:"language-bash line-numbers-mode","data-ext":"sh"},[s("pre",{class:"language-bash"},[s("code",null,[s("span",{class:"token comment"},"# install in your project"),n(`
+`),s("span",{class:"token function"},"pnpm"),n(),s("span",{class:"token function"},"add"),n(),s("span",{class:"token parameter variable"},"-D"),n(` vuepress@next @vuepress/client@next vue
+
+`),s("span",{class:"token comment"},"# create a markdown file"),n(`
+`),s("span",{class:"token builtin class-name"},"echo"),n(),s("span",{class:"token string"},"'# Hello VuePress'"),n(),s("span",{class:"token operator"},">"),n(` README.md
+
+`),s("span",{class:"token comment"},"# start writing"),n(`
+`),s("span",{class:"token function"},"pnpm"),n(` vuepress dev
+
+`),s("span",{class:"token comment"},"# build to static files"),n(`
+`),s("span",{class:"token function"},"pnpm"),n(` vuepress build
+`)])]),s("div",{class:"line-numbers","aria-hidden":"true"},[s("div",{class:"line-number"}),s("div",{class:"line-number"}),s("div",{class:"line-number"}),s("div",{class:"line-number"}),s("div",{class:"line-number"}),s("div",{class:"line-number"}),s("div",{class:"line-number"}),s("div",{class:"line-number"}),s("div",{class:"line-number"}),s("div",{class:"line-number"}),s("div",{class:"line-number"})])],-1),p=s("div",{class:"language-bash line-numbers-mode","data-ext":"sh"},[s("pre",{class:"language-bash"},[s("code",null,[s("span",{class:"token comment"},"# install in your project"),n(`
+`),s("span",{class:"token function"},"yarn"),n(),s("span",{class:"token function"},"add"),n(),s("span",{class:"token parameter variable"},"-D"),n(` vuepress@next
+
+`),s("span",{class:"token comment"},"# create a markdown file"),n(`
+`),s("span",{class:"token builtin class-name"},"echo"),n(),s("span",{class:"token string"},"'# Hello VuePress'"),n(),s("span",{class:"token operator"},">"),n(` README.md
+
+`),s("span",{class:"token comment"},"# start writing"),n(`
+`),s("span",{class:"token function"},"yarn"),n(` vuepress dev
+
+`),s("span",{class:"token comment"},"# build to static files"),n(`
+`),s("span",{class:"token function"},"yarn"),n(` vuepress build
+`)])]),s("div",{class:"line-numbers","aria-hidden":"true"},[s("div",{class:"line-number"}),s("div",{class:"line-number"}),s("div",{class:"line-number"}),s("div",{class:"line-number"}),s("div",{class:"line-number"}),s("div",{class:"line-number"}),s("div",{class:"line-number"}),s("div",{class:"line-number"}),s("div",{class:"line-number"}),s("div",{class:"line-number"}),s("div",{class:"line-number"})])],-1),b=s("div",{class:"language-bash line-numbers-mode","data-ext":"sh"},[s("pre",{class:"language-bash"},[s("code",null,[s("span",{class:"token comment"},"# install in your project"),n(`
+`),s("span",{class:"token function"},"npm"),n(),s("span",{class:"token function"},"install"),n(),s("span",{class:"token parameter variable"},"-D"),n(` vuepress@next
+
+`),s("span",{class:"token comment"},"# create a markdown file"),n(`
+`),s("span",{class:"token builtin class-name"},"echo"),n(),s("span",{class:"token string"},"'# Hello VuePress'"),n(),s("span",{class:"token operator"},">"),n(` README.md
+
+`),s("span",{class:"token comment"},"# start writing"),n(`
+npx vuepress dev
+
+`),s("span",{class:"token comment"},"# build to static files"),n(`
+npx vuepress build
+
+`)])]),s("div",{class:"line-numbers","aria-hidden":"true"},[s("div",{class:"line-number"}),s("div",{class:"line-number"}),s("div",{class:"line-number"}),s("div",{class:"line-number"}),s("div",{class:"line-number"}),s("div",{class:"line-number"}),s("div",{class:"line-number"}),s("div",{class:"line-number"}),s("div",{class:"line-number"}),s("div",{class:"line-number"}),s("div",{class:"line-number"}),s("div",{class:"line-number"})])],-1);function k(h,f){const i=c("CodeTabs");return r(),o("div",null,[m,d(i,{id:"3",data:[{id:"PNPM"},{id:"YARN"},{id:"NPM"}],active:0,"tab-id":"shell"},{title0:e(({value:a,isActive:l})=>[n("PNPM")]),title1:e(({value:a,isActive:l})=>[n("YARN")]),title2:e(({value:a,isActive:l})=>[n("NPM")]),tab0:e(({value:a,isActive:l})=>[v]),tab1:e(({value:a,isActive:l})=>[p]),tab2:e(({value:a,isActive:l})=>[b]),_:1})])}const g=t(u,[["render",k],["__file","index.html.vue"]]);export{g as default};

499a389c18b4d129ccaed8593641888358237ec3
Mister-Hope <mister-hope@outlook.com>
2024-02-06 17:19:42 +0000

    Deploying to netlify from @ vuepress/docs@c21de3940eb8ed6dc5f7dae1ee48d76bc9371ab1 🚀

---
+++ assets/cli.html-3BNS0WzA.js
@@ -0,0 +1,46 @@
+import{_ as r}from"./plugin-vue_export-helper-x3n3nnut.js";import{r as t,o as d,c as o,a as n,b as e,d as s,w as c,e as i}from"./app-un-IDmnc.js";const m={},u=i(`<h1 id="command-line-interface" tabindex="-1"><a class="header-anchor" href="#command-line-interface"><span>Command Line Interface</span></a></h1><p>Run <code>vuepress --help</code> to get following help messages:</p><div class="language-text line-numbers-mode" data-ext="text" data-title="text"><pre class="language-text"><code>Usage:
+  $ vuepress &lt;command&gt; [options]
+
+Commands:
+  dev [sourceDir]    Start development server
+  build [sourceDir]  Build to static site
+  info               Display environment information
+
+For more info, run any command with the \`--help\` flag:
+  $ vuepress dev --help
+  $ vuepress build --help
+  $ vuepress info --help
+
+Options:
+  -v, --version  Display version number
+  -h, --help     Display this message
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,3),p={class:"hint-container tip"},v=n("p",{class:"hint-container-title"},"Tips",-1),h={href:"https://www.npmjs.com/package/debug",target:"_blank",rel:"noopener noreferrer"},b=n("p",null,[e("Set environment variable "),n("code",null,"DEBUG=vuepress*"),e(" to enable debug logs.")],-1),f=i(`<h2 id="dev" tabindex="-1"><a class="header-anchor" href="#dev"><span>dev</span></a></h2><p>Start a development server to develop your VuePress site locally.</p><div class="language-text line-numbers-mode" data-ext="text" data-title="text"><pre class="language-text"><code>Usage:
+  $ vuepress dev [sourceDir]
+
+Options:
+  -c, --config &lt;config&gt;  Set path to config file
+  -p, --port &lt;port&gt;      Use specified port (default: 8080)
+  -t, --temp &lt;temp&gt;      Set the directory of the temporary files
+  --host &lt;host&gt;          Use specified host (default: 0.0.0.0)
+  --cache &lt;cache&gt;        Set the directory of the cache files
+  --clean-temp           Clean the temporary files before dev
+  --clean-cache          Clean the cache files before dev
+  --open                 Open browser when ready
+  --debug                Enable debug mode
+  --no-watch             Disable watching page and config files (default: true)
+  -v, --version          Display version number
+  -h, --help             Display this message
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><div class="hint-container tip"><p class="hint-container-title">Tips</p><p>Options set by CLI will override those options with the same name in your config file.</p></div><h2 id="build" tabindex="-1"><a class="header-anchor" href="#build"><span>build</span></a></h2>`,5),g=i(`<div class="language-text line-numbers-mode" data-ext="text" data-title="text"><pre class="language-text"><code>Usage:
+  $ vuepress build [sourceDir]
+
+Options:
+  -c, --config &lt;config&gt;  Set path to config file
+  -d, --dest &lt;dest&gt;      Set the directory build output (default: .vuepress/dist)
+  -t, --temp &lt;temp&gt;      Set the directory of the temporary files
+  --cache &lt;cache&gt;        Set the directory of the cache files
+  --clean-temp           Clean the temporary files before build
+  --clean-cache          Clean the cache files before build
+  --debug                Enable debug mode
+  -v, --version          Display version number
+  -h, --help             Display this message
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><div class="hint-container tip"><p class="hint-container-title">Tips</p><p>Options set by CLI will override those options with the same name in your config file.</p></div><h2 id="info" tabindex="-1"><a class="header-anchor" href="#info"><span>info</span></a></h2><p>Outputs information about your system and dependencies.</p><p>This command would be helpful when you want to check your environment or report an issue.</p>`,5);function y(_,x){const l=t("ExternalLinkIcon"),a=t("RouteLink");return d(),o("div",null,[u,n("div",p,[v,n("p",null,[e("VuePress is using "),n("a",h,[e("debug"),s(l)]),e(" module.")]),b]),f,n("p",null,[e("Build your VuePress site to static files, which are ready for "),s(a,{to:"/guide/deployment.html"},{default:c(()=>[e("deployment")]),_:1}),e(".")]),g])}const C=r(m,[["render",y],["__file","cli.html.vue"]]),S=JSON.parse('{"path":"/reference/cli.html","title":"Command Line Interface","lang":"en-US","frontmatter":{"icon":"bi:terminal-fill","description":"Command Line Interface Run vuepress --help to get following help messages: Tips VuePress is using debug module. Set environment variable DEBUG=vuepress* to enable debug logs. de...","head":[["link",{"rel":"alternate","hreflang":"zh-cn","href":"https://vuejs.press/zh/reference/cli.html"}],["meta",{"property":"og:url","content":"https://vuejs.press/reference/cli.html"}],["meta",{"property":"og:site_name","content":"VuePress"}],["meta",{"property":"og:title","content":"Command Line Interface"}],["meta",{"property":"og:description","content":"Command Line Interface Run vuepress --help to get following help messages: Tips VuePress is using debug module. Set environment variable DEBUG=vuepress* to enable debug logs. de..."}],["meta",{"property":"og:type","content":"article"}],["meta",{"property":"og:locale","content":"en-US"}],["meta",{"property":"og:locale:alternate","content":"zh-CN"}],["meta",{"property":"og:updated_time","content":"2024-02-06T14:32:58.000Z"}],["meta",{"property":"article:modified_time","content":"2024-02-06T14:32:58.000Z"}],["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"Article\\",\\"headline\\":\\"Command Line Interface\\",\\"image\\":[\\"\\"],\\"dateModified\\":\\"2024-02-06T14:32:58.000Z\\",\\"author\\":[]}"]]},"headers":[{"level":2,"title":"dev","slug":"dev","link":"#dev","children":[]},{"level":2,"title":"build","slug":"build","link":"#build","children":[]},{"level":2,"title":"info","slug":"info","link":"#info","children":[]}],"git":{"createdTime":1701614677000,"updatedTime":1707229978000,"contributors":[{"name":"Mr.Hope","email":"mister-hope@outlook.com","commits":3},{"name":"meteorlxy","email":"meteor.lxy@foxmail.com","commits":3}]},"readingTime":{"minutes":1.1,"words":330},"filePathRelative":"reference/cli.md","localizedDate":"December 3, 2023","autoDesc":true}');export{C as comp,S as data};
